e7a6ea94264189c80b4d4646dd12e491ef4cd6a9
krishnasri555 <krishnasri56692@gmail.com>
2025-07-19 22:22:58 +0530

    fc

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router>
+    <Router basename="cicd-exp1">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
